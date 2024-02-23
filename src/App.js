65671d086887f25746ea4d6036c729bb0bf53def
lilansheng <945004642@qq.com>
2024-02-24 05:22:07 +0800

    JSX中实现复杂条件渲染

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-const isLogin = true
-// const isLogin = false
+const articleType = 2 // 0 1 2
+
+function getArticleTem() {
+  if (articleType === 0) {
+    return <div>我是无图模式</div>
+  } else if (articleType === 1) {
+    return <div>我是单图模式</div>
+  } else {
+    return <div>我是二图模式</div>
+  }
+}
+
 function App() {
-  return (
-    <div className="App">
-      {/* && */}
-      {isLogin && <span>this is span</span>}
-      {/* 三元运算 */}
-      {isLogin ? <span>jack</span> : <span>login...</span>}
-    </div>
-  )
+  return <div className="App">{getArticleTem()}</div>
 }
 
 export default App
