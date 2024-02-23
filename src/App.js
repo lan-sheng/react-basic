2068b68c219bd8658316968d2bf3aa0d00272f00
lilansheng <945004642@qq.com>
2024-02-24 04:52:12 +0800

    Jsx中实现列表渲染

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-const count = 100
-
-function geName() {
-  return 'jack'
-}
-
+const list = [
+  { id: 1001, name: 'vue1' },
+  { id: 1002, name: 'vue2' },
+  { id: 1003, name: 'vue3' },
+]
 function App() {
   return (
     <div className="App">
       this is app
-      {/* 使用引号传递字符串 */}
-      {'this is message'}
-      {/* 识别js变量 */}
-      {count}
-      {/* 函数调用 */}
-      {geName()}
-      {/* 方法调用 */}
-      {new Date().getDate()}
-      {/* 使用js对象 */}
-      <div style={{ color: 'red' }}>this is dav</div>
+      <ul>
+        {list.map(item => <li key={item.id}>{ item.name}</li>)}
+     </ul>
     </div>
   )
 }
