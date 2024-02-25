04d18edcb67e402265dcc4303435b2be865ff118
lilansheng <945004642@qq.com>
2024-02-25 12:56:48 +0800

    useState修改状态的规则

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
-  
+  let [count, setCount] = useState(0)
+
   const handleClick = () => {
-    setCount(count+1)
+    setCount(count + 1)
+    // count++
+    console.log('count: ', count)
   }
+
+  const [form, setForm] = useState({ name: 'aaa' })
+
+  const changeForm = () => {
+    // form.name='bbb'
+    setForm({ ...form, name: 'bbb' })
+  }
+
   return (
     <div className="App">
       <button onClick={handleClick}>{count}</button>
+      <button onClick={changeForm}>修改form{form.name}</button>
     </div>
   )
 }
