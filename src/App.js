29deca3b3499e1f8520b2608889c45e770124572
lilansheng <945004642@qq.com>
2024-02-25 12:19:00 +0800

    React组件基础使用

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-// const handleClick = () => {
-//   console.log('button被点击了')
+// function Button() {
+//   return <button>click me</button>
 // }
-// 事件参数
-// const handleClick = (e) => {
-//   console.log('button被点击了',e)
-// }
-// 传递自定义参数
-// const handleClick = name => {
-//   console.log('button被点击了', name)
-// }
-// 既传递自定义参数又传递事件对象e
-const handleClick = (name, e) => {
-  console.log('button被点击了', name, e)
-}
+const Button=()=><button>click me</button>
+
 function App() {
   return (
     <div className="App">
-      <button onClick={e => handleClick('aaa', e)}>click me</button>
+      <Button />
+      <Button></Button>
     </div>
   )
 }
