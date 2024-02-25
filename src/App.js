68937d4dd82976f6b2be2a8f4ec88076e304aee1
lilansheng <945004642@qq.com>
2024-02-25 13:17:43 +0800

    基础样式控制

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import { useState } from 'react'
-
+import './index.css';
+const style = {
+  color: 'red',
+  fontSize: '50px',
+}
 function App() {
-  let [count, setCount] = useState(0)
-
-  const handleClick = () => {
-    setCount(count + 1)
-    // count++
-    console.log('count: ', count)
-  }
-
-  const [form, setForm] = useState({ name: 'aaa' })
-
-  const changeForm = () => {
-    // form.name='bbb'
-    setForm({ ...form, name: 'bbb' })
-  }
-
   return (
     <div className="App">
-      <button onClick={handleClick}>{count}</button>
-      <button onClick={changeForm}>修改form{form.name}</button>
+      {/* <span style={{ color: 'red',fontSize:'30px ' }}>aaa</span> */}
+      <span style={style}>aaa</span>
+      <span className="bbb">bbb</span>
     </div>
   )
 }
