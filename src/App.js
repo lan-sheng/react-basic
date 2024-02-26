1b793c1062750d5200669ec856a006c47a9ee333
lilansheng <945004642@qq.com>
2024-02-27 00:17:47 +0800

    父传子

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import { useRef } from 'react'
-
+function Son(props) {
+  console.log('props: ', props)
+  return (
+    <div>
+      this is son，{props.name}，{props.child}，{props.children}
+    </div>
+  )
+}
 function App() {
-  const inputRef = useRef(null)
-  const showDom = () => {
-    console.log(inputRef.current)
-  }
+  const name = 'this is app name '
   return (
     <div className="App">
-      <input type="text" ref={inputRef} />
-      <button onClick={showDom}>获取dom</button>
+      <Son name={name} age={18} isTrue={false} list={['vue', 'react']} cb={() => console.log(1)} child={<span>123</span>} >
+        <span>222</span>
+      </Son>
     </div>
   )
 }
