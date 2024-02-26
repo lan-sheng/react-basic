a2ef48017fb1edd16379266f8c638e9617f87496
lilansheng <945004642@qq.com>
2024-02-27 00:42:38 +0800

    兄弟组件通信

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import { useState } from 'react'
 
-function Son({ onGetSonMsg }) {
-  const sonMsg = 'Son'
+function A({ onGetAName }) {
+  const name = 'aaa'
   return (
     <div>
-      this is Son
-      <button onClick={() => onGetSonMsg(sonMsg)}>sendMsg</button>
+      this is aaa
+      <button onClick={() => onGetAName(name)}>发送</button>
     </div>
   )
 }
+function B({ name }) {
+  return <div>this is bbb，{name}</div>
+}
 function App() {
-  const [msg, setMsg] = useState('')
-  const getMsg = msg => {
-    console.log('msg: ', msg)
-    setMsg(msg)
+  const [name, setName] = useState('')
+  const getAName = name => {
+    console.log('name: ', name)
+    setName(name)
   }
   return (
     <div className="App">
       this is app
-      <Son onGetSonMsg={getMsg}></Son>
-      {msg}
+      <A onGetAName={getAName} />
+      <B name={name} />
     </div>
   )
 }
