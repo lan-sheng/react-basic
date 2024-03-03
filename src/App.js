460bcb80f16196667a76c3de144680f01acf8801
lilansheng <945004642@qq.com>
2024-03-03 13:32:29 +0800

    异步请求提交异步数据

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import { useDispatch, useSelector } from 'react-redux'
 import { inscrement, decrement, addToNum } from './store/modules/counterStore'
+import { fetchChannlList } from './store/modules/channelStore'
+import { useEffect } from 'react'
 function App() {
   const { count } = useSelector(state => state.counter)
+  const { channelList } = useSelector(state => state.channel)
   const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(fetchChannlList())
+  }, [dispatch])
   return (
     <div className="App">
       <button onClick={() => dispatch(decrement())}>-</button>
@@ -10,6 +16,11 @@ function App() {
       <button onClick={() => dispatch(inscrement())}>+</button>
       <button onClick={() => dispatch(addToNum(10))}>变10</button>
       <button onClick={() => dispatch(addToNum(20))}>变20</button>
+      <ul>
+        {channelList.map(item => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
     </div>
   )
 }
